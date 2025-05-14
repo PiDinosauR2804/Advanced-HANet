fdf32199245f42de269b1e05c68cd130580c330f
nmquang003 <sinper003@gmail.com>
2025-05-14 10:12:18 +0700

    fix optune to finetune ace on base

--- optune.py
+++ optune.py
@@ -9,66 +9,75 @@ args = parse_arguments()
 
 def objective(trial):
     # Hằng 
-    args.epochs = 50
-    args.use_lora = True
     args.logs_dir = "logs/classifier"
     args.data_root = "./data/data_ids"
-    args.dataset = "MAVEN"
+    args.dataset = "ACE"
     args.backbone = "bert-base-uncased"
     args.decay = 1e-4
     args.no_freeze_bert = True
     args.shot_num = 5
+    args.class_num = 10
     args.device = "cuda:0"
     args.log = True
     args.log_dir = "./outputs/log_incremental/temp7_submax/first_wo_UCL+TCL/"
     args.log_name = "ashuffle_lnone_r1"
-    args.dweight_loss = True
-    args.rep_aug = "mean"
-    args.distill = "pd"
-    args.class_num = 20
     args.single_label = True
-    args.cl_aug = "shuffle"
-    args.aug_repeat_times = 3
-    args.joint_da_loss = "ce"
+    ####
+    # args.dweight_loss = True
+    # args.rep_aug = "mean"
+    # args.distill = "pd"
+    # args.cl_aug = "shuffle"
+    # args.joint_da_loss = "ce"
+    # args.aug_repeat_times = 3
+    # args.sub_max = True
+    # args.cl_temp = 0.07
+    args.dweight_loss = trial.suggest_categorical("dweight_loss", [True, False])
+    args.rep_aug = trial.suggest_categorical("rep_aug", ["mean", "relative", "none"])
+    args.distill = trial.suggest_categorical("distill", ["fd", "pd", "mul", "none"])
+    args.cl_aug = trial.suggest_categorical("cl_aug", ["dropout", "shuffle", "RTR", "none"])
+    args.joint_da_loss = trial.suggest_categorical("joint_da_loss", ["none", "ce", "dist", "mul"])
+    args.aug_repeat_times = trial.suggest_int("aug_repeat_times", 2, 5)
     args.sub_max = True
-    args.cl_temp = 0.07
+    args.cl_temp = trial.suggest_float("cl_temp", 0.05, 0.5, step=0.01)
+    ####
     args.tlcl = False
     args.ucl = True
     args.skip_first_cl = "ucl+tlcl"
     args.use_description = True
     args.num_description = 3
     args.ratio_loss_des_cl = 1
-    args.task_ep_time = 1
+    args.epochs = 10
+    args.task_ep_time = 4
     args.early_stop = True
     args.skip_eval_ep = 0
     args.eval_freq = 1
     args.patience = 4
-    args.early_stop = True
     args.classifier_layer = 1
     args.hidden_dim = 128
     args.dropout = 0.5
     args.use_general_expert = True
-    args.use_mole = True
+    args.use_lora = False
+    args.use_mole = False
     args.step_size = 1
     args.wandb = True
     args.eval_batch_size = 64
     args.task_ep_time = 1
     args.uniform_ep = 3
     args.lora_dropout = 0.3
-    args.batch_size = 8
-    args.project_name = "HANet_mole_bert_full"
+    args.project_name = "HANet_ace_2_5_full"
 
     # Tham số cho Optuna trial
     args.lr = trial.suggest_float("lr", 5e-5, 2e-4, log=True)
-    args.lora_rank = trial.suggest_categorical("lora_rank", [64, 128, 256])
-    args.lora_alpha = trial.suggest_int("lora_alpha", 32, 128, step=32)
-    args.mole_num_experts = trial.suggest_categorical("mole_num_experts", [4, 8])
-    args.mole_top_k = trial.suggest_categorical("mole_top_k", [2, 4])
+    args.batch_size = trial.suggest_categorical("batch_size", [4, 8])
+    # args.lora_rank = trial.suggest_categorical("lora_rank", [64, 128, 256])
+    # args.lora_alpha = trial.suggest_int("lora_alpha", 32, 128, step=32)
+    # args.mole_num_experts = trial.suggest_categorical("mole_num_experts", [4, 8])
+    # args.mole_top_k = trial.suggest_categorical("mole_top_k", [2, 4])
         
-    args.gammalr = trial.suggest_float("gamma", 0.8, 1.0, step=0.01)
-    args.entropy_weight = trial.suggest_float("entropy_weight", 0.1, 1, step=0.1)
-    args.load_balance_weight = trial.suggest_float("load_balance_weight", 0.1, 1.0, step=0.1)
-    args.general_expert_weight = trial.suggest_float("general_expert_weight", 0.1, 1.0, step=0.1)
+    # args.gammalr = trial.suggest_float("gamma", 0.8, 1.0, step=0.01)
+    # args.entropy_weight = trial.suggest_float("entropy_weight", 0.1, 1, step=0.1)
+    # args.load_balance_weight = trial.suggest_float("load_balance_weight", 0.1, 1.0, step=0.1)
+    # args.general_expert_weight = trial.suggest_float("general_expert_weight", 0.1, 1.0, step=0.1)
     
     try:
         f1 = train(0, args, trial)
