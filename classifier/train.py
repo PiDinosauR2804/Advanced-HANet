ce72d1b9e83fd8f4f91b9a87b0cecf92c51fa967
nmquang003 <sinper003@gmail.com>
2025-05-14 15:53:13 +0700

    fix eval

--- classifier/train.py
+++ classifier/train.py
@@ -701,7 +701,8 @@ def train(local_rank, args, trial=None):
                         })
             scheduler.step()
             logger.info(f"Num choose: {num_choose}")
-            if ((ep + 1) % int(args.eval_freq*ep_time) == 0 and args.early_stop and ((ep + 1) >= args.skip_eval_ep*ep_time or stage > 0)) or (ep + 1) == num_epochs: # TODO TODO
+            if ((ep + 1) % max(int(args.eval_freq*ep_time), 1) == 0 and args.early_stop and ((ep + 1) >= args.skip_eval_ep*ep_time or stage > 0)) \
+                or (ep + 1) == num_epochs: # TODO TODO
                 # Evaluation process
                 logger.info("Evaluation process ...")
                 model.eval()
