6a738e4940170347c7d510c3e2241d0c7704d57a
nmquang003 <sinper003@gmail.com>
2025-05-25 11:02:36 +0700

    add stage lr ratio

--- configs.py
+++ configs.py
@@ -53,6 +53,7 @@ def parse_arguments():
     parser.add_argument("--warmup_ep", type=int, default=0, help="Warmup steps for learning rate")
     parser.add_argument("--warmup_ratio", type=float, default=0.1, help="Warmup ratio for learning rate")
     parser.add_argument("--min_lr_ratio", type=float, default=0.1, help="Minimum lr ratio")
+    parser.add_argument("--stage_lr_ratio", type=float, default=1.0, help="Stage learning rate ratio")
     parser.add_argument("--scheduler", type=str, default="lambda", choices=["lambda", "cyclic", "step"], help="Scheduler type for learning rate")
     parser.add_argument("--lambda_type", type=str, default="batch", choices=["epoch", "batch"], help="Type of lambda scheduler")
     # loss = loss_ce + loss_ucl * args.ratio_loss_ucl + \
