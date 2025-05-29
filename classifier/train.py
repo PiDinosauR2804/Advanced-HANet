c609267c224f86d1d9b522d479ee427c53e478eb
nmquang003 <sinper003@gmail.com>
2025-05-29 11:40:33 +0700

    add log precision and recall

--- classifier/train.py
+++ classifier/train.py
@@ -884,7 +884,7 @@ def train(local_rank, args, trial=None):
                     })
                     
                     
-                    logger.info(f'marco F1 {micro_F1}')
+                    logger.info(f'marco F1 {micro_F1} | precision {precision} | recall {recall}')
                     # dev_scores_ls.append(micro_F1)
                     # logger.info(f"Dev scores list: {dev_scores_ls}")
                     logger.info(f"bc:{bc}")
