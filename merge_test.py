f237ca861964f365a684b0d653a6e89d981cd80f
nmquang003 <sinper003@gmail.com>
2025-05-30 09:45:34 +0700

    tmp

--- merge_test.py
+++ merge_test.py
@@ -1,5 +1,6 @@
 import os
 import json
+import numpy as np
 
 dataset = 'ACE'
 
@@ -12,16 +13,25 @@ with open(os.path.join(original_path, dataset, f"{dataset}.test.jsonl"), 'r') as
 with open(os.path.join(span_path, dataset, f"{dataset}.test.jsonl"), 'r') as f:
     span_test = [json.loads(line) for line in f]
 
-for oitem, sitem in zip(original_test, span_test):
-    label_mask = []
-    for sp in oitem['span']:
-        if sp in sitem['span']:
-            label_mask.append(True)
-        else:
-            label_mask.append(False)
+# for oitem, sitem in zip(original_test, span_test):
+#     label_mask = []
+#     for sp in oitem['span']:
+#         if sp in sitem['span']:
+#             label_mask.append(True)
+#         else:
+#             label_mask.append(False)
             
-    oitem["label_mask"] = label_mask
+#     oitem["label_mask"] = label_mask
     
-with open(os.path.join(original_path, dataset, f"{dataset}.test.jsonl"), 'w') as f:
-    for item in original_test:
-        f.write(json.dumps(item) + "\n")
+# with open(os.path.join(original_path, dataset, f"{dataset}.test.jsonl"), 'w') as f:
+#     for item in original_test:
+#         f.write(json.dumps(item) + "\n")
+
+o_len = 0
+s_len = 0
+for oitem, sitem in zip(original_test, span_test):
+    o_len += len(oitem['span'])
+    s_len += len(sitem['span'])
+    
+print(f"Original span length: {o_len}")
+print(f"Span span length: {s_len}")
