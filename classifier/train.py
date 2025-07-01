a2d82e2be7482286407ebc296e5a0404865f4ac0
nmquang003 <sinper003@gmail.com>
2025-07-01 11:02:31 +0700

    fix print eval pre

--- classifier/train.py
+++ classifier/train.py
@@ -822,8 +822,8 @@ def train(local_rank, args, trial=None):
                         print(
                             "=============================",
                             f"Text: {tokenizer.decode(eval_x[0]).replace('[PAD]', '')}",
-                            f"Label: {eval_y}",
-                            f"Prediction: {eval_prediction.item()}",
+                            f"Label: {eval_y[:10]}",
+                            f"Prediction: {eval_prediction[:10]}",
                             "=============================",
                             sep="\n"
                         )
