dfd30815e3c8597c91d2c367184cc82cd38616f7
nmquang003 <sinper003@gmail.com>
2025-07-22 16:06:01 +0700

    stack hs output

--- classifier/model.py
+++ classifier/model.py
@@ -342,7 +342,7 @@ class BertED(nn.Module):
         # return_dict['cur_token_imp']      = cur_token_imp.detach()
         return_dict['imp_mask']    = imp_mask
         # return_dict['cur_feat_tokens_imp'] = cur_feat_tokens_imp.detach()
-        return_dict['cur_feat_tokens_imp'] = expert_outputs_hs
+        return_dict['cur_feat_tokens_imp'] = torch.stack(expert_outputs_hs)
         return_dict['topk_indices'] = topk_indices
 
 
