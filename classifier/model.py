f8ec0c225715f64d416b79cb1e5d8939cd0b07b4
nmquang003 <sinper003@gmail.com>
2025-07-22 16:33:20 +0700

    fix return imp mask

--- classifier/model.py
+++ classifier/model.py
@@ -343,7 +343,8 @@ class BertED(nn.Module):
                         # for b in range(B)]
 
         # return_dict['cur_token_imp']      = cur_token_imp.detach()
-        return_dict['imp_mask']    = imp_masks_tmp
+        if imp_masks is None:
+            return_dict['imp_mask']    = imp_masks_tmp
         # return_dict['cur_feat_tokens_imp'] = cur_feat_tokens_imp.detach()
         return_dict['cur_feat_tokens_imp'] = torch.stack(expert_outputs_hs)
         return_dict['topk_indices'] = topk_indices
